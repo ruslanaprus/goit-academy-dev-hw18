f6d0c159286cf73f1b1cfabb6a946a6ad2e4b9ba
ruslana <ruslana.prus@ukr.net>
2024-12-25 01:12:42 +0100

    remove redundant code

--- src/main/java/com/example/notemanager/api/config/ApiSecurityConfig.java
+++ src/main/java/com/example/notemanager/api/config/ApiSecurityConfig.java
@@ -1,22 +1,16 @@
 package com.example.notemanager.api.config;
 
 import com.example.notemanager.api.security.JwtRequestFilter;
-import com.example.notemanager.model.User;
-import com.example.notemanager.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -45,20 +39,4 @@ public class ApiSecurityConfig {
                 .build();
     }
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(UserService userService,
-                                                            @Qualifier("passEncoder") PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(username -> {
-            User user = userService.findByUserName(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            return org.springframework.security.core.userdetails.User
-                    .withUsername(user.getUsername())
-                    .password(user.getPassword())
-                    .authorities(user.getRole())
-                    .build();
-        });
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
-    }
 }
